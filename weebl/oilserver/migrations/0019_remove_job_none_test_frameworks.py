8b7fd6be5f1b542e732c694bd48ec1729d1f730f
Darren Hoyland <darren.hoyland@canonical.com>
2016-05-17 16:21:38 +0100

    Fix for integration database: A migration to find testcaseinstances that have, for example, 'pipeline_deploy_none' as their build test framework, when it should be 'pipeline_deploy_notapplicable' (the none and notapplicable being the spot where the version string goes for non-build testframeworks). [Merge] ~autonomouse/weebl:bugfix/migration into weebl:master

---
+++ weebl/oilserver/migrations/0019_remove_job_none_test_frameworks.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def get_or_create(model, params):
+    if model.objects.filter(**params).exists():
+        correct_model = model.objects.get(**params)
+    else:
+        correct_model = model(**params)
+        correct_model.save()
+    return correct_model
+
+def get_or_create_correct_test_framework(jobtype):
+    params = {'name': jobtype.name, 'version': 'notapplicable'}
+    return get_or_create(models.TestFramework, params)
+
+def get_or_create_correct_correct_testcaseclass(jobtype, test_framework):
+    params = {'name': jobtype.name, 'testframework': test_framework}
+    return get_or_create(models.TestCaseClass, params)
+
+def get_or_create_correct_correct_testcase(jobtype, testcaseclass):
+    params = {'name': jobtype.name, 'testcaseclass': testcaseclass}
+    return get_or_create(models.TestCase, params)
+
+def remove_job_none_test_frameworks(apps, schema_editor):
+    """This method finds testcaseinstances that have the build as the name of
+    their test_framework (so "pipeline_deploy", for example) and "none" as
+    their version (which would mean that the test_framework would be identified
+    as "pipeline_deploy_none" in the above example). It then removes this and
+    instead links it to the test_framework with "notapplicable" as the version
+    (i.e. "pipeline_deploy_notapplicable").
+
+    The reason for this is that the version field is only applicable for
+    non-build (i.e. test) testframeworks, so we use "notapplicable" as the
+    version for build testframeworks. However, in some earlier cases, "None"
+    was mistakenly used (i.e. "pipeline_deploy_none"). This method finds and
+    replaces them so that they all use the "notapplicable" as the version.
+
+    """
+
+    TestCaseInstance = apps.get_model('oilserver', 'TestCaseInstance')
+    params = {'testcase__testcaseclass__testframework__version__isnull': True}
+    for old_tci in TestCaseInstance.objects.filter(**params):
+        build_id = old_tci.build.build_id
+        jobtype = old_tci.build.jobtype
+        pipeline = old_tci.build.pipeline
+        status = old_tci.testcaseinstancestatus
+        # N.B. returned_build is the same as what the weeblclient method
+        # 'get_build_uuid_from_build_id_job_and_pipeline' fetches:
+        returned_build = apps.get_model('oilserver', 'Build').objects.get(
+            build_id=build_id, jobtype=jobtype, pipeline=pipeline)
+        correct_test_framework = get_or_create_correct_test_framework(jobtype)
+        correct_testcaseclass = get_or_create_correct_correct_testcaseclass(
+            jobtype, correct_test_framework)
+        correct_testcase = get_or_create_correct_correct_testcase(
+            jobtype, correct_testcaseclass)
+        TestCaseInstance.objects.create(build=returned_build,
+                                         testcase=correct_testcase,
+                                         status=status)
+        old_tci.delete()
+    TestCaseInstance.objects.filter(**params).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oilserver', '0018_auto_20160504_1015'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            remove_job_none_test_frameworks
+        ),
+    ]
