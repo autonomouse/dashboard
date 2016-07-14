d8cb2c34040941520cc86745a4b011639b22283d
Nuno Santos <nuno.santos@canonical.com>
2016-07-13 20:12:06 -0400

    submit xml tempest results to weebl [Merge] ~nunosantos/weebl:master

---
+++ weebl/oilserver/migrations/0022_add_skipped_testcaseinstancestatus.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from oilserver.models import TestCaseInstanceStatus
+
+def add_skipped_testcaseinstancestatus(apps, schema_editor):
+    ''' Add the "skipped" status to the testcaseinstancestatus.'''
+    skipped = TestCaseInstanceStatus(name='skipped',
+                                     description='The build or test was skipped.')
+    skipped.save()
+    return skipped
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oilserver', '0021_auto_20160525_1439'),
+    ]
+    operations = [
+        migrations.RunPython(
+            add_skipped_testcaseinstancestatus
+        ),
+    ]
