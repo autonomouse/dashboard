c06621c5d732af3f80f01116d46198da9e01f4d9
Darren Hoyland <darren.hoyland@canonical.com>
2017-01-09 17:39:33 +0000

    Changed authorisation so that no one can see any content unless they have been made into 'staff'. Members of the OIL team will have to be made into a 'superuser' for admin access - useful when we move it to the public facing oil.canonical.com. Merge ~autonomouse/weebl:dev/make_only_staff_see_content into weebl:master

--- weebl/oilserver/api/authorization.py
+++ weebl/oilserver/api/authorization.py
@@ -1,10 +1,19 @@
 from tastypie.exceptions import Unauthorized
 from tastypie.authorization import DjangoAuthorization
+from django.contrib.auth.models import User
 
 
 class WorldReadableDjangoAuthorization(DjangoAuthorization):
     """Allow all authenticated users to read from the API endpoints"""
+    def is_staff_check(self, bundle):
+        username = bundle.request.user
+        user = User.objects.get(username=username)
+        if not (user.is_staff or user.is_superuser):
+            raise Unauthorized("You are not allowed to access that resource.")
+
     def read_list(self, object_list, bundle):
+        self.is_staff_check(bundle)
+
         if self.base_checks(bundle.request, object_list.model) is False:
             return []
 
@@ -12,6 +21,8 @@ class WorldReadableDjangoAuthorization(DjangoAuthorization):
         return object_list
 
     def read_detail(self, object_list, bundle):
+        self.is_staff_check(bundle)
+
         if self.base_checks(bundle.request, bundle.obj.__class__) is False:
             raise Unauthorized("You are not allowed to access that resource.")
 
