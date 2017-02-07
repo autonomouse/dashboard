7e6e1af74304d7ea6f58dd02dcec97b6a99d4ca7
Darren Hoyland <darren.hoyland@canonical.com>
2017-02-07 09:31:46 +0000

    Added a migration to merge machines with other machines of the same name but with a .oil(staging) suffix. This originally crept in due to the push_bundle_info method in weeblclient not removing any .oil suffix. Merge ~autonomouse/weebl:migration/merge_machines_with_oil_suffix into weebl:master

---
+++ weebl/oilserver/migrations/0039_remove_oil_suffix_form_machines_and_merge.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import os
+from django.db import migrations, transaction
+
+
+def remove_oil_suffix_from_machines_and_merge(apps, schema_editor):
+    '''Find machines that end in a .oil suffix and merge them with the real one
+    (i.e. the one without the .oil).'''
+
+    with transaction.atomic():
+        Machine = apps.get_model('oilserver', 'Machine')
+        MachineConfiguration = apps.get_model(
+            'oilserver', 'MachineConfiguration')
+        oily_machines = [machine for machine in Machine.objects.all() if '.' in
+                         machine.hostname] # (to find *.oil and *.oilstaging)
+        for machine_oil in oily_machines:
+            correct_machine_name = os.path.splitext(machine_oil.hostname)[0]
+            [correct_machine, _] = Machine.objects.get_or_create(
+                hostname=correct_machine_name)
+            incorrect_machine_configs = MachineConfiguration.objects.filter(
+                machine=machine_oil)
+            for bad_config in incorrect_machine_configs:
+                bad_config.machine = correct_machine
+                bad_config.save()
+            machine_oil.delete()
+
+
+def noop():
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oilserver',
+         '0038_add_data_from_initial_settings_fixture_with_colours'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_oil_suffix_from_machines_and_merge,
+                             reverse_code=noop),
+    ]
