fdd297dd194860fb1151268e52be7f1eed788646
Greg Lutostanski <gregory.lutostanski@canonical.com>
2017-02-09 13:12:35 -0600

    Set success to None for old models

--- weebl/oilserver/migrations/0047_auto_20170209_1413.py
+++ weebl/oilserver/migrations/0047_auto_20170209_1413.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import migrations, transaction, models
+
+def set_success_to_none(apps, schema_editor):
+    """So that we know what historical data is correct, set the success value
+    to None for starting"""
+    from oilserver.models import JujuServiceDeployment
+    with transaction.atomic():
+        for deployment in JujuServiceDeployment.objects.all():
+            deployment.success = None
+            deployment.save()
 
 
 class Migration(migrations.Migration):
@@ -16,4 +25,5 @@ class Migration(migrations.Migration):
             name='success',
             field=models.NullBooleanField(help_text='Whether this juju service deployed successfully.', default=None),
         ),
+        migrations.RunPython(set_success_to_none),
     ]
